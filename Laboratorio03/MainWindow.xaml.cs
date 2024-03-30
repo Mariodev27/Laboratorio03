d5ad7ae7af449157033fc0e74b7e5d1cf499fabc
Alumno <Alumno@LAB1504-03>
2024-03-30 12:55:34 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio03/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Laboratorio03
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            dvPrueba.ItemsSource = GetStudents().DefaultView;
+        }
+
+        public DataTable GetStudents()
+        {
+            string connectionString = "Data Source=LAB1504-03\\SQLEXPRESS;Initial Catalog=BDMario ;User Id=Mario;" + "Password=1234";
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataTable);
+            }
+
+            return dataTable;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Laboratorio03/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio03
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class StudentRepository
+    {
+        public List<Student> GetStudents()
+        {
+            string connectionString = "Data Source=LAB1504-03\\SQLEXPRESS;Initial Catalog=BDMario;User Id=Mario;Password=1234";
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Student student = new Student
+                        {
+                            StudentId = reader.GetInt32(reader.GetOrdinal("StudentId")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                        };
+
+                        students.Add(student);
+                    }
+                }
+            }
+
+            return students;
+        }
+    }
+}
